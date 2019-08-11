0c4f45f71ec5f7f3ed6f37a039d33e6830b4b47e
prakharpks02 <prakharpks02@gmail.com>
2019-08-11 21:03:36 +0530

    CSS added

--- TMoS_ERP/TMoS_ERP/urls.py
+++ TMoS_ERP/TMoS_ERP/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
-    path('logout/', auth_views.LogoutView, name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'), name='logout'),
     path('home/', home_view.home, name='home'),
     path('', home_view.home, name='home'),
 ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
