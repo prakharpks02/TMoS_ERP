6c940d8d82d24fbdf29ccea6d132b0f975af220a
anish509 <s.anish509@gmail.com>
2019-09-10 19:28:15 +0530

    Allowed Hosts changed

--- TMoS_ERP/settings.py
+++ TMoS_ERP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vnd=f!!2@4-g-!5cuiu2a8_6%e0c*zve69hvck#nys70l235*%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
