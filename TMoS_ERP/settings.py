af86b94d8e2a21c9639eafaf365f899a3bb4844e
anish509 <s.anish509@gmail.com>
2019-09-11 01:57:48 +0530

    Allowed Hosts changed

--- TMoS_ERP/settings.py
+++ TMoS_ERP/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = '/home/'
 LOGIN_URL = '/login/'
